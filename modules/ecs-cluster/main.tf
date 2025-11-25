a2e49b17766d3dd7cd3902fb890b64b13209d009
Xlink-Development <mohan.bhimavarapu@ioco.tech>
2025-11-25 15:50:33 +0530

    new changes

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -65,7 +65,35 @@ resource "aws_iam_role_policy_attachment" "task_execution_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# ALB (optional)
+resource "aws_lb" "alb" {
+  name               = "${var.name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.ecs_sg.id]
+}
 
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+  health_check {
+    path = "/"
+    matcher = "200,301,302"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
 
 # outputs
 output "cluster_name" {
@@ -79,6 +107,9 @@ output "public_subnets" {
 }
 output "security_group_id" {
   value = aws_security_group.ecs_sg.id
+}
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
 }
 output "task_execution_role_arn" {
   value = aws_iam_role.task_exec.arn
